32d36705867ed292b7e7c06c6b55481541bd3433
lijie <271349348@qq.com>
2018-11-24 13:09:25 +0800

    写了一些样式与几个页面的模板

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'express' });
 });
+
+router.get('/users.html',function(req,res){
+  res.render('users');
+});
+
+router.get('/brand.html',function(req,res){
+  res.render('brand');
+});
+
+router.get('/phone.html',function(req,res){
+  res.render('phone');
+})
 
 module.exports = router;
